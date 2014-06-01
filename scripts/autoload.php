13b5066ffa531ef84d6152908fb151b0b308818e
Chris Wright <daverandom@php.net>
2014-06-01 13:03:50 +0000

    Updated autoloader to work on *nix

--- scripts/autoload.php
+++ scripts/autoload.php
@@ -4,11 +4,12 @@
 
 spl_autoload_register(function($className) {
     static $path;
+
     if (!isset($path)) {
         $path = realpath(__DIR__ . '/../src/');
     }
 
     if (strtolower(substr($className, 0, 12)) === 'phpdocsearch') {
-        require $path . '/' . $className . '.php';
+        require $path . '/' . strtr($className, '\\', '/') . '.php';
     }
 });
